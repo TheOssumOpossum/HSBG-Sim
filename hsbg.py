a696d6fb0c848a4e8a47ac623fb9c2724a9464c6
TheOssumOpossum <v.bommier@gmail.com>
2019-12-06 02:34:08 -0600

    add mass simulator and csv recording

--- hsbg.py
+++ hsbg.py
@@ -1,4 +1,6 @@
 import random
+import copy
+import csv
 
 class Minion:
 	def __init__(self, name, att, hp, stars=1, taunt=False, deathrattle=[], deatheffect=[], shield=False):
@@ -37,6 +39,7 @@ class Minion:
 	def buff(self, att=0, hp=0):
 		self.att += att
 		self.hp += hp
+		return self
 
 class DeathEffect:
 	def __init__(self, death_effect_id):
@@ -106,11 +109,11 @@ class Result:
 
 	def __str__(self):
 		string = ""
-		if self.target == 1:
+		if self.target == -1:
 			string += "Win! They "
-		elif self.target == 0:
+		elif self.target == 1:
 			string += "Loss! You "
-		else:
+		else: #self.target == 0
 			string += "Tie! Both "
 		string += "took " + str(self.damage) + " damage."
 		return string
@@ -138,9 +141,9 @@ def combat(bottom_board, top_board, seed=[]):
 		bottom_fight = not bottom_fight
 	if len(bottom_board) == 0:
 		if len(top_board) == 0:
-			return Result(-1, 0)
+			return Result(0, 0)
 		else:
-			return Result(0,top_board.get_damage())
+			return Result(-1,top_board.get_damage())
 	if len(top_board) == 0:
 		return Result(1,bottom_board.get_damage())
 	print("COMBAT FINISHED WITHOUT A WINNER")
@@ -233,12 +236,126 @@ def create_minion(minion_id=None, name=None):
 		hp = 1
 	return Minion(name, att, hp, stars=stars, taunt=taunt, deathrattle=deathrattle, deatheffect=deatheffect, shield=shield)
 
-their_board = Board()
-their_board.summon(Minion("shield",2,5, deatheffect=[DeathEffect(1)]))
-their_board.summon(Minion("one",1,5))
-my_board = Board()
-my_board.summon(Minion("Amalgam",1,5))
-result = combat(my_board, their_board, [0,0])
-print(their_board)
-print(my_board)
-print(result)
+def simulate(bottom_board_number, top_board_number, seeds=None, simulations=1000):
+	bottom_board = build_board(bottom_board_number)
+	top_board = build_board(top_board_number)
+	bottom_board.cleanup()
+	top_board.cleanup()
+	results = {}
+	if seeds is None:
+		seeds = [[random.randrange(0,2)] for i in range(simulations)]
+	for seed in seeds:
+		bottom = copy.deepcopy(bottom_board)
+		top = copy.deepcopy(top_board)
+		result = combat(bottom, top, seed)
+		dmg = result.damage * result.target
+		if dmg in results:
+			results[dmg] = results[dmg] + 1
+		else:
+			results[dmg] = 1
+	return results
+
+def build_board(board_number):
+	board = Board()
+	if board_number == 0: #straight buy
+		board.summon(create_minion(1))
+		board.summon(create_minion(2))
+	elif board_number == 1:
+		board.summon(create_minion(3))
+	elif board_number == 2:
+		board.summon(create_minion(4))
+	elif board_number == 3:
+		board.summon(create_minion(5))
+	elif board_number == 4:
+		board.summon(create_minion(6))
+	elif board_number == 5:
+		board.summon(create_minion(8))
+	elif board_number == 6:
+		board.summon(create_minion(9))
+	elif board_number == 7:
+		board.summon(create_minion(10))
+		board.summon(create_minion(11))
+	elif board_number == 8:
+		board.summon(create_minion(12))
+	elif board_number == 9:
+		board.summon(create_minion(13))
+	elif board_number == 10:
+		board.summon(create_minion(14))
+	elif board_number == 11:
+		board.summon(create_minion(15))
+	elif board_number == 12: #brann
+		board.summon(create_minion(1).buff(1,1))
+		board.summon(create_minion(2))
+	elif board_number == 13:
+		board.summon(create_minion(5).buff(1,1))
+	elif board_number == 14:
+		board.summon(create_minion(10).buff(1,1))
+		board.summon(create_minion(11))
+	elif board_number == 15:
+		board.summon(create_minion(11).buff(1,1))
+		board.summon(create_minion(10))
+	elif board_number == 16:
+		board.summon(create_minion(12))
+	elif board_number == 17: #yogg
+		board.summon(create_minion(1).buff(1,1))
+		board.summon(create_minion(2))
+	elif board_number == 18:
+		board.summon(create_minion(3).buff(1,1))
+	elif board_number == 19:
+		board.summon(create_minion(4).buff(1,1))
+	elif board_number == 20:
+		board.summon(create_minion(5).buff(1,1))
+	elif board_number == 21:
+		board.summon(create_minion(6).buff(1,1))
+	elif board_number == 22:
+		board.summon(create_minion(8).buff(1,1))
+	elif board_number == 23:
+		board.summon(create_minion(9).buff(1,1))
+	elif board_number == 24:
+		board.summon(create_minion(10).buff(1,1))
+		board.summon(create_minion(11))
+	elif board_number == 25:
+		board.summon(create_minion(11).buff(1,1))
+		board.summon(create_minion(10))
+	elif board_number == 26:
+		board.summon(create_minion(12).buff(1,1))
+	elif board_number == 27:
+		board.summon(create_minion(13).buff(1,1))
+	elif board_number == 28:
+		board.summon(create_minion(14).buff(1,1))
+	elif board_number == 29:
+		board.summon(create_minion(15).buff(1,1))
+		#rat king
+		#curator
+		#rag
+		#nefarian
+		#lich king
+		#mech girl
+		#patches
+		#sir finley
+		#queen tagwoggle
+		#AFK
+	return board
+
+def run_simulations():
+	results = {}
+	for i in range(13,14):
+		for j in range(i,12):
+			results[i,j] = simulate(i,j)
+	return results
+
+# their_board = Board()
+results = run_simulations()
+print(results)
+
+# with open('test.csv', 'w') as f:
+    # for key in results.keys():
+        # f.write("%s,%s\n"%(key,results[key]))
+
+# their_board.summon(Minion("Murloc",1,1))
+# their_board.summon(Minion("Murloc Tidecaller",2,2))
+# print(their_board)
+# my_board = Board()
+# my_board.summon(Minion("Direwolf Alpha",1,1))
+# my_board.summon(Minion("Direwolf Alpha",2,2))
+# results = simulate(my_board, their_board, simulations=10)
